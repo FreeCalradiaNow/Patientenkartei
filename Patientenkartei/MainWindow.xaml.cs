eb01545529b96e43e2c2890da72607b4424180ae
m.witzik <m.witzik@B-WRK-0224>
2022-08-23 14:29:38 +0200

    Logik hinzugefügt btnCreate_Click speichert jetzt den Inhalt von textBoxContent als "Inhalt von textBoxFileName.txt"

--- Patientenkartei/MainWindow.xaml.cs
+++ Patientenkartei/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Windows;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Patientenkartei
 {
@@ -27,7 +16,16 @@ namespace Patientenkartei
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
+            string content = textBoxContent.Text;
+            string filename = textBoxFileName.Text;
 
+
+            using (FileStream fs = File.Create(filename + ".txt"))
+            {
+                byte[] contentconvertedToBytes = Encoding.ASCII.GetBytes(content);
+                fs.Write(contentconvertedToBytes, 0, contentconvertedToBytes.Length);
+
+            }
         }
     }
 }
